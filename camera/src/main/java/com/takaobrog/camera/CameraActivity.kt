d0fb7b30af26b3fcc318ecaf42854b34324bf8ef
kimuratakuou <ruia1082halfnc@gmail.com>
2026-02-11 10:54:41 +0900

    権限チェック実装

--- camera/src/main/java/com/takaobrog/camera/CameraActivity.kt
+++ camera/src/main/java/com/takaobrog/camera/CameraActivity.kt
@@ -1,21 +1,35 @@
 package com.takaobrog.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CameraActivity : AppCompatActivity() {
 
-    private val cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
-        if (it != null) {
-            Log.d("CameraActivity", it.toString());
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { result ->
+            if (result != null) {
+                Log.d("CameraActivity", result.toString());
+            }
         }
-    }
+
+    private val requestCameraPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                launchCamera()
+            } else {
+                Toast.makeText(this, "カメラ権限が必要です", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +42,26 @@ class CameraActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btnCamera).setOnClickListener {
-            cameraLauncher.launch(null)
+            checkCameraPermissionAndLaunch()
         }
+    }
+
+    private fun checkCameraPermissionAndLaunch() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                launchCamera()
+            }
+
+            else -> {
+                requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private fun launchCamera() {
+        cameraLauncher.launch(null)
     }
 }
